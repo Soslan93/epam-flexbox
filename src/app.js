de1165b519fe6c196839f451bed10f9120ed63fe
Soslan <kulumbekov1807@gmail.com>
2018-06-17 20:00:57 +0300

    epam-flexbox

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './scss/header.scss';
+import './scss/main.scss';
+import './scss/slider.scss';
+import './scss/intro-content.scss';
+import './scss/gallery.scss';
+import './scss/footer.scss';
+import './scss/copyright.scss';
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+let path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname,"./dist"),
+        filename: "main.js",
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    //fallback: "style-loader",
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ] 
+                })
+                /*use: [
+                    'style-loader',
+                    'css-loader'
+                ]*/
+            }
+
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css")
+    ]
+}
